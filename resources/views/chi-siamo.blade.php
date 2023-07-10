7bf873d9abd6d4b2523f6024bad33ce10c82b361
Vadim-Cr <vadim.crudu@gmail.com>
2023-07-10 23:38:04 +0200

    first commit

---
+++ resources/views/chi-siamo.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>
+        CHI SIAMO!
+    </h2>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, ratione fuga quae dolores amet, incidunt ex laudantium hic magni assumenda nam quis, reiciendis et dolore voluptates eos odit repellat aut! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur, ullam voluptates repudiandae quis totam placeat dolorum tempora aliquam nemo mollitia? Maiores laborum labore asperiores praesentium excepturi commodi fugit nemo eaque!
+    </p>
+</body>
+</html>
